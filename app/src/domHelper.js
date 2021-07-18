235d8a83503d296fd9b56d282aa309084e7b7276
Mars <diablo_13_06@mail.ru>
2021-07-18 22:03:12 +0300

    add: DOMHelper

---
+++ app/src/domHelper.js
@@ -0,0 +1,59 @@
+module.exports = class DomHelper {
+  // преобразует из строки в виртуальный dom, с помощью встроенного в js класса
+  static parseStrToDom(str) {
+    const parser = new DOMParser();
+    return parser.parseFromString(str, "application/xml");
+  }
+
+  // оборачивает все текстовые ноды в придуманный спец тег <text-editor>
+  static wrapTextNodes(dom) {
+    const body = dom.body;
+
+    let textNodes = [];
+
+    function recursion(element) {
+      element.childNodes.forEach((node) => {
+        // replace(/\s+/g, '') заменяет переносы и пробелы
+        if (
+          node.nodeName === "#text" &&
+          node.nodeValue.replace(/\s+/g, "").length > 0
+        ) {
+          textNodes.push(node);
+        } else {
+          recursion(node);
+        }
+      });
+    }
+
+    recursion(body);
+
+    // оборачиваем все текстовые узлы в свой специальный тег для редактирования
+    // добавляем этому тегу возможность редактировать контент
+    textNodes.forEach((node, i) => {
+      const wrapper = dom.createElement("text-editor");
+
+      node.parentNode.replaceChild(wrapper, node);
+      wrapper.appendChild(node);
+      wrapper.contentEditable = true;
+      // присваиваем каждой текстовой ноде id, чтобы связать ее в дальнейшем с виртуальным dom
+      wrapper.setAttribute("node_id", i);
+
+      // ? node.parentNode.contentEditable = true;
+    });
+
+    return dom;
+  }
+
+  // преобразует html-документ в строку, с помощью встроенного в js класса
+  static serializeDomToString(dom) {
+    const serializer = new XMLSerializer();
+    return serializer.serializeToString(dom);
+  }
+
+  static unwrapTextNodes(dom) {
+    dom.body.querySelectorAll("text-editor").forEach((element) => {
+      // берем текстовый узел и помещаем его вместо тега text-editor вместе с этим узлом
+      element.parentNode.replaceChild(element.firstChild, element);
+    });
+  }
+};
--- app/src/editor.js
+++ app/src/editor.js
@@ -1,42 +1,54 @@
+const axios = require("axios");
+const DOMHelper = require("./domHelper");
 // эта библиотека нужна для того, чтобы обойти кэширование у браузера
 // обход кэширования происходит за счет добавления каждый рах рандомных данных
-require('./iframe-load');
+require("./iframe-load");
 
 module.exports = class Editor {
   constructor() {
     // находим iframe в документе и помещаем его в свойство iframe нашего класса
-    this.iframe = document.querySelector('iframe');
+    this.iframe = document.querySelector("iframe");
   }
 
-  open(page){
-    this.iframe.load(`../${page}`, () => {
-      const body = this.iframe.contentDocument.body;
+  open(page) {
+    this.currentPage = page;
 
-      let textNodes = [];
-
-      function recursion(element) {
-        element.childNodes.forEach( node => {
-          // replace(/\s+/g, '') заменяет переносы и пробелы
-          if(node.nodeName === '#text' && node.nodeValue.replace(/\s+/g, '').length > 0){
-            textNodes.push(node);
-          } else {
-            recursion(node);
-          }
-        })
-      }
-
-      recursion(body);
-
-      // оборачиваем все текстовые узлы в свой специальный тег для редактирования
-      // добавляем этому тегу возможность редактировать контент
-      textNodes.forEach(node => {
-        const wrapper = this.iframe.contentDocument.createElement('text-editor');
-
-        node.parentNode.replaceChild(wrapper, node);
-        wrapper.appendChild(node);
-        wrapper.contentEditable = true;
-        // node.parentNode.contentEditable = true;
+    axios
+      .get("../" + page)
+      .then((res) => DOMHelper.parseStrToDom(res.data))
+      .then(DOMHelper.wrapTextNodes)
+      .then((dom) => {
+        this.virtualDom = dom; // создаем переменную virtualDpm и записываем в нее dom
+        return dom;
       })
-    });
+      .then(DOMHelper.serializeDomToString)
+      .then((html) => axios.post("./api/saveTempPage.php", { html }))
+      .then(() => this.iframe.load("../temp.html"))
+      .then(() => this.enableEditing());
   }
-}
+
+  // включить редактирование контента у всех наших текстовых тегов
+  enableEditing() {
+    this.iframe.contentDocument.body
+      .querySelectorAll("text-editor")
+      .forEach((element) => {
+        element.contentEditable = true;
+        element.addEventListener("input", () => {
+          this.onTextEdit(element);
+        });
+      });
+  }
+
+  onTextEdit(element) {
+    const id = element.getAttribute("node_id");
+    this.virtualDom.body.querySelector(`[node_id="${id}"]`).innerHTML =
+      element.innerHTML;
+  }
+
+  save() {
+    const newDom = this.virtualDom.cloneNode(this.virtualDom);
+    DOMHelper.unwrapTextNodes(newDom);
+    const html = DOMHelper.serializeDomToString(newDom);
+    axios.post("./api/savePage.php", { pageName: this.currentPage, html });
+  }
+};
